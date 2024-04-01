53f68b0b47b652f5b1eb34c2be13f9b538ac4cd8
PuriA <ankur.puri@rabobank.com>
2024-04-01 18:02:19 +0530

    Added script to save generated data into database tables instead of csv files. It took 18 minutes, same as saving in csv files.

--- create_multitable_synthetic_data_motor_vehicle_stolen.py
+++ create_multitable_synthetic_data_motor_vehicle_stolen.py
@@ -1,13 +1,12 @@
 import os
 
+from sqlalchemy import create_engine
 import pandas as pd
 from sdv.multi_table import HMASynthesizer  # Hierarchical Modeling Algorithm Synthesizer
 from sdv.metadata import MultiTableMetadata
 from sdv.datasets.local import load_csvs
 from sdv.evaluation.multi_table import run_diagnostic, evaluate_quality
 from sdv.evaluation.multi_table import get_column_plot
-import pymysql
-
 
 # Read multitable data from multiple CSV files
 # real_data = load_csvs(
@@ -18,11 +17,9 @@ import pymysql
 #     })
 
 
-# Read input data from local SQL server database instead of csv's using pymsql library
+# Read input data from local SQL server database instead of csv's using pyodbc library
 import pyodbc
 
-# conn_str = f'DRIVER={{ODBC SQL Server Driver}};SERVER=localhost;DATABASE=GenAI_POCDB;UID=ankurp;PWD=Yetochalega@123
-# # ';TrustServerCertificate=True
 conn_str = ("Driver={SQL Server};"
             "Server=AVD-PRD-SI-008;"
             "Database=GenAI_POCDB;"
@@ -31,7 +28,6 @@ conn_str = ("Driver={SQL Server};"
 conn = pyodbc.connect(conn_str)
 
 # Get tables within the database
-
 try:
     with conn.cursor() as cursor:
         # Read all tables within the database
@@ -39,9 +35,14 @@ try:
         list_tables = cursor.fetchall()
         table_row = list_tables
         table_list = [i for i in table_row]
+
+        # Delete any output tables present in database, as otherwise input data will also read those output tables
         final_table_list = []
         for column in table_list:
-            final_table_list.append(column[0])
+            if '_out' in column[0]:
+                pass
+            else:
+                final_table_list.append(column[0])
 
         real_data_dict = {}
         # Create a dataframe for each table
@@ -59,91 +60,98 @@ try:
                     final_dict.update(df_dict)
 
                 temp_df = pd.DataFrame(final_dict, index=[0])
-                df_merged = pd.concat([df_merged,temp_df], ignore_index=True)
+                df_merged = pd.concat([df_merged, temp_df], ignore_index=True)
 
             # Create merged dictionary of dataframes for each table
-            table_dict = {table : df_merged}
+            table_dict = {table: df_merged}
             real_data_dict.update(table_dict)
         real_data = real_data_dict
+
+    # Create a Multitable Metadata object
+    metadata = MultiTableMetadata()
+
+    # # Automatically detect the metadata based on actual data
+    # metadata.detect_from_csvs(
+    #     folder_name='Motor+Vehicle+Thefts+CSV'
+    # )
+
+    # Automatically detect the metadata based on actual data
+    metadata.detect_from_dataframes(real_data)
+
+    # Check if metadata is correct
+    metadata_dict = metadata.to_dict()
+    try:
+        metadata.save_to_json('multitable_metadata_motor_vehicle_theft.json')
+    except ValueError:
+        os.remove('multitable_metadata_motor_vehicle_theft.json')
+        metadata.save_to_json('multitable_metadata_motor_vehicle_theft.json')
+
+    # Check for columns with unknown sdtype and fix them, this needs to be done manually based on input dataset
+    # Otherwise, model gives error - ValueError: invalid literal for int() with base 10:
+    metadata.update_column(table_name='locations', column_name='region', sdtype='categorical')
+    metadata.update_column(table_name='locations', column_name='population', sdtype='numerical')
+    metadata.update_column(table_name='license_details', column_name='License_ID', sdtype='id')
+    metadata.update_column(table_name='stolen_vehicles', column_name='License_ID', sdtype='id')
+    # metadata.update_column(table_name='make_details', column_name='make_name', sdtype='categorical')
+    # metadata.update_column(table_name='stolen_vehicles', column_name='vehicle_desc', sdtype='categorical')
+    metadata.update_column(table_name='owner', column_name='name', sdtype='categorical')
+    metadata.update_column(table_name='owner', column_name='address', sdtype='categorical')
+
+    # Add relationship between tables manually if metadata object doesn't detect automatically
+    metadata.add_relationship(parent_table_name='license_details', child_table_name='stolen_vehicles',
+                              parent_primary_key='License_ID', child_foreign_key='License_ID')
+
+    # # Visualize the metadata
+    metadata.visualize(
+        show_table_details='full',
+        show_relationship_labels=True,
+        output_filepath='multitable_metadata_motor_vehicle_theft.png'
+    )
+
+    # Validate the metadata API
+    metadata.validate()
+
+    # Train the model and generate synthetic data
+
+    # Step 1: Create the synthesizer
+    synthesizer = HMASynthesizer(metadata)
+
+    synthesizer.validate(real_data)
+
+    # Step 2: Train the synthesizer
+    synthesizer.fit(real_data)
+
+    # Step 3: Generate synthetic data
+    synthetic_data = synthesizer.sample(scale=1)
+
+    # Saving data in csv files
+    # for table_name, synthetic_dataframe in synthetic_data.items():
+    #     print("Saving data in csv file for: " + str(table_name))
+    #     synthetic_dataframe.to_csv(str(table_name) + "_out" + ".csv", index=False)
+
+    # Saving data in database in output tables
+    engine = create_engine('mssql+pyodbc://AVD-PRD-SI-008/GENAI_POCDB?driver=ODBC+Driver+17+for+SQL+Server')
+
+    for table_name, synthetic_dataframe in synthetic_data.items():
+        synthetic_dataframe.to_sql(table_name + str('_out'), con=engine, if_exists='replace', index=False)
 
 finally:
     conn.close()
 
-# Create a Multitable Metadata object
-metadata = MultiTableMetadata()
-
-# # Automatically detect the metadata based on actual data
-# metadata.detect_from_csvs(
-#     folder_name='Motor+Vehicle+Thefts+CSV'
-# )
-
-# Automatically detect the metadata based on actual data
-metadata.detect_from_dataframes(real_data)
-
-# Check if metadata is correct
-metadata_dict = metadata.to_dict()
-try:
-    metadata.save_to_json('multitable_metadata_motor_vehicle_theft.json')
-except ValueError:
-    os.remove('multitable_metadata_motor_vehicle_theft.json')
-    metadata.save_to_json('multitable_metadata_motor_vehicle_theft.json')
-#
-# Check for columns with unknown sdtype and fix them, this needs to be done manually based on input dataset
-# Otherwise, model gives error - ValueError: invalid literal for int() with base 10:
-# metadata.update_column(table_name='locations', column_name='region', sdtype='categorical')
-metadata.update_column(table_name='locations', column_name='population', sdtype='numerical')
-metadata.update_column(table_name='license_details', column_name='License_ID', sdtype='id')
-metadata.update_column(table_name='stolen_vehicles', column_name='License_ID', sdtype='id')
-# metadata.update_column(table_name='make_details', column_name='make_name', sdtype='categorical')
-# metadata.update_column(table_name='stolen_vehicles', column_name='vehicle_desc', sdtype='categorical')
-# metadata.update_column(table_name='owner', column_name='name', sdtype='categorical')
-# metadata.update_column(table_name='owner', column_name='address', sdtype='categorical')
-
-# Add relationship between tables manually if metadata object doesn't detect automatically
-metadata.add_relationship(parent_table_name='license_details', child_table_name='stolen_vehicles',
-                          parent_primary_key='License_ID', child_foreign_key='License_ID')
-
-# # Visualize the metadata
-metadata.visualize(
-    show_table_details='full',
-    show_relationship_labels=True,
-    output_filepath='multitable_metadata_motor_vehicle_theft.png'
-)
-#
-# Validate the metadata API
-metadata.validate()
-#
-# Train the model and generate synthetic data
-#
-# # Step 1: Create the synthesizer
-synthesizer = HMASynthesizer(metadata)
-#
-synthesizer.validate(real_data)
-#
-# Step 2: Train the synthesizer
-synthesizer.fit(real_data)
-#
-# # Step 3: Generate synthetic data
-synthetic_data = synthesizer.sample(scale=1)
-#
-for table_name, synthetic_dataframe in synthetic_data.items():
-    print("Saving data in csv file for: " + str(table_name))
-    synthetic_dataframe.to_csv(str(table_name) + "_out" + ".csv", index=False)
-
 # 1. perform basic validity checks
-print("Performing diagnostics for basic validity checks...")
-diagnostic = run_diagnostic(real_data, synthetic_data, metadata)
-
-# 2. measure the statistical similarity
-print("Evaluating statistical similarity between real data and synthetic data generated...")
-
-quality_report = evaluate_quality(real_data, synthetic_data, metadata)
-
-# Which columns have high and low scores
-quality_report.get_details('Column Shapes')
-quality_report.get_details('Column Pair Trends')
-quality_report.get_details('Cardinality')
-quality_report.get_details('Intertable Trends')
+# print("Performing diagnostics for basic validity checks...")
+# diagnostic = run_diagnostic(real_data, synthetic_data, metadata)
+#
+# # 2. measure the statistical similarity
+# print("Evaluating statistical similarity between real data and synthetic data generated...")
+#
+# quality_report = evaluate_quality(real_data, synthetic_data, metadata)
+#
+# # Which columns have high and low scores
+# quality_report.get_details('Column Shapes')
+# quality_report.get_details('Column Pair Trends')
+# quality_report.get_details('Cardinality')
+# quality_report.get_details('Intertable Trends')
 
 # 3. plot the data
 # fig = get_column_plot(
